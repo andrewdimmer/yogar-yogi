1225e718399a1e89fc809502bcce047277f36c40
Andrew Dimmer <contactandrewd@gmail.com>
2020-06-14 14:43:05 -0400

    Updates

---
+++ precache-manifest.90d08eb140aecb3d2652f2852bf588a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "090faaea860698c577df3fd556861d7b",
+    "url": "/yogar-yogi/index.html"
+  },
+  {
+    "revision": "5f8a1c59526212df5cc3",
+    "url": "/yogar-yogi/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "b7ea104db4144494b2e2",
+    "url": "/yogar-yogi/static/js/2.125fe97b.chunk.js"
+  },
+  {
+    "revision": "4d47e5a861cd7a1c063193cfcc8827b7",
+    "url": "/yogar-yogi/static/js/2.125fe97b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f8a1c59526212df5cc3",
+    "url": "/yogar-yogi/static/js/main.bb61d70d.chunk.js"
+  },
+  {
+    "revision": "b4ad425b8d10e873cc97",
+    "url": "/yogar-yogi/static/js/runtime-main.374ee10b.js"
+  }
+]);
